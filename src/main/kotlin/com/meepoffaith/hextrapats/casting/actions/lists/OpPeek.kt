845cfdc305d7a0e0c3d1f9a641669dd7d137994f
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2026-07-14 18:01:04 -0700

    Bug fix

--- src/main/kotlin/com/meepoffaith/hextrapats/casting/actions/lists/OpPeek.kt
+++ src/main/kotlin/com/meepoffaith/hextrapats/casting/actions/lists/OpPeek.kt
@@ -4,13 +4,16 @@ import at.petrak.hexcasting.api.casting.castables.ConstMediaAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.getList
 import at.petrak.hexcasting.api.casting.iota.Iota
-import at.petrak.hexcasting.api.casting.iota.ListIota
+import at.petrak.hexcasting.api.casting.iota.NullIota
 
 class OpPeek (val peekEnd: Boolean) : ConstMediaAction {
     override val argc = 1
     override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> {
-        val list = args.getList(0, argc).toMutableList()
-        val datum = if(peekEnd) list.last() else list.first()
-        return listOf(ListIota(list), datum)
+        val list = args.getList(0, argc)
+        if(list.nonEmpty){
+            val datum = if (peekEnd) list.last() else list.first()
+            return listOf(args[0], datum)
+        }
+        return listOf(args[0], NullIota())
     }
 }
