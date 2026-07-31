316560fac27bed6604d1f7c8367e18d88570cad8
MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
2026-07-31 14:16:02 -0700

    Missing lang for invalid set inputs

--- src/common/main/kotlin/com/meepoffaith/hextrapats/casting/actions/sets/OpListToSet.kt
+++ src/common/main/kotlin/com/meepoffaith/hextrapats/casting/actions/sets/OpListToSet.kt
@@ -4,12 +4,22 @@ import at.petrak.hexcasting.api.casting.castables.ConstMediaAction
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.getList
 import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.casting.mishaps.MishapInvalidIota
+import com.meepoffaith.hextrapats.casting.iota.IotaMap
 import com.meepoffaith.hextrapats.casting.iota.SetIota
 
 object OpListToSet : ConstMediaAction {
     override val argc = 1
     override fun execute(args: List<Iota>, env: CastingEnvironment): List<Iota> {
-        val iotas = args.getList(0, argc).toMutableList()
-        return SetIota(iotas, -1).asActionResult()
+        val iotas = args.getList(0, argc)
+        val map = IotaMap()
+        for(iota in iotas){
+            if(IotaMap.checkType(iota)){
+                map.addIota(iota)
+            }else{
+                throw MishapInvalidIota.of(iota, 0, "hextrapats:set_item_list")
+            }
+        }
+        return map.asActionResult()
     }
 }
--- src/common/main/kotlin/com/meepoffaith/hextrapats/casting/iota/IotaMap.kt
+++ src/common/main/kotlin/com/meepoffaith/hextrapats/casting/iota/IotaMap.kt
@@ -7,9 +7,9 @@ import com.meepoffaith.hextrapats.util.MathUtils
 class IotaMap : HashMap<Int, Iota> {
     constructor(): super()
     constructor(map: IotaMap) : super(map)
-    constructor(list: List<Iota>, argcOrIdx: Int = 0) : super(){
+    constructor(list: List<Iota>, argc: Int = 0) : super(){
         for(i in 0..list.lastIndex){
-            addIota(list[i], if (argcOrIdx > 0) argcOrIdx - (i + 1) else argcOrIdx)
+            addIota(list[i], argc - (i + 1))
         }
     }
 
@@ -24,11 +24,7 @@ class IotaMap : HashMap<Int, Iota> {
 
     fun addIota(iota: Iota, reversedIdx: Int): Boolean{
         if(!checkType(iota)) {
-            if(reversedIdx >= 0) {
-                throw MishapInvalidIota.of(iota, reversedIdx, "hextrapats:set_item")
-            }else{
-                throw MishapInvalidIota.of(iota, -reversedIdx + 1, "hextrapats:list_in_list_to_set")
-            }
+            throw MishapInvalidIota.of(iota, reversedIdx, "hextrapats:set_item")
         }
         return addIota(iota)
     }
